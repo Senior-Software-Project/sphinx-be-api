cb3e348c1f4efc6ba3d71fbb02be5b67336b42c5
gordong-msud <gordong@msudenver.edu>
2022-04-03 15:59:58 -0600

    copied current state from friendly-barnacle

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-/*
+/* Code is based on material presented in Pluralsight course, "Play by Play: Building a Node Web API with Sam Artioli and John Papa" at:
+        https://app.pluralsight.com/library/courses/play-by-play-node-web-api-john-papa-sam-artioli/table-of-contents (subscription req'd)
+*/
+
 import express from 'express'
+import bodyParser from 'body-parser'
+
+import userStats from './user-stats.js'
 
 const app = express()
 
 const hostname = '127.0.0.1'
 const port = 3000
 
-app.get('/', (req, res) => {
-  // res.send('Hello World!');
-  res.json('hello: world')
-})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+const stats = userStats(app)
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+/*   userStatsRoutesLoaded = false
+  if (typeof stats != "undefined") {
+    userStatsRoutesLoaded = true
+  } */
+  if (typeof stats === 'undefined') {
+    console.log(`Server running at http://${hostname}:${port}/, userStats routes loaded`)
+  } else {
+    console.log(`Server running at http://${hostname}:${port}/`)
+  }
 })
-
-/*
-import { createServer } from 'http';
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-}).listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-*/
